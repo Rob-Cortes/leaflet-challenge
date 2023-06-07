3854aaa900809c122e1fe171e8207e7576be175a
Rob-Cortes <rob.collins.cortes@gmail.com>
2023-06-07 10:24:54 -0400

    Added popup information

--- static/js/logic.js
+++ static/js/logic.js
@@ -12,32 +12,43 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-
+let geojson;
 
 // Fetch GeoJSON data
 d3.json(url).then(function(data) {
 
     console.log(data);
+    function chooseColor(depth) {
+      if (depth < 1) return "yellow";
+      else if (depth < 3) return "gold";
+      else if (depth < 5) return "orange";
+      else if (depth < 7) return "darkorange";
+      else if (depth < 9) return "firebrick";
+      else return "darkred";
+    };
 
     function createStyle(feature) {
       return {
-        color: 'Orange',
+        color: chooseColor(feature.geometry.coordinates[2]),
         radius: feature.properties.mag*5,
-        fillOpacity: feature.geometry.coordinates[2]/15
+        fillOpacity: 1
       }
-    }
+    };
     
     // create a vector circle centered on each point feature's latitude and longitude
     function createCircles (feature, latlng) {
       return L.circleMarker(latlng, createStyle(feature))
-    }
+    };
     
     // create an options object that specifies which function will called on each feature
     var myLayerOptions = {
-      pointToLayer: createCircles
-    }
-    
+      pointToLayer: createCircles,
+      // This is called on each feature.
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup("<h1>" + feature.properties.place + "</h1> <hr> <h3>Mag: " + feature.properties.mag + "</h3><h3>Depth: " + feature.geometry.coordinates[2] + "</h3><h3>Time: " + new Date(feature.properties.time) + "</h3>");
+      }
+    };
     // create the GeoJSON layer from the myLayerData object (not shown in this snippet)
-    L.geoJSON(data, myLayerOptions).addTo(myMap)
+    geojson = L.geoJSON(data, myLayerOptions).addTo(myMap);
 
 });
